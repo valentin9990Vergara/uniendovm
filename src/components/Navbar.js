d8e575e9b6bcb90cb4b46e9ba22609f7d19af016
valentin9990Vergara <valentin999vergara@outlook.com>
2024-10-14 17:48:08 -0300

    logo grande, colot nav y footer, boton afil

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 import logo from '../assets/logo.png'; // Importa el logo
 import whatsappIcon from '../assets/whatsapp.png'; // Importa el ícono de WhatsApp
 import instagramIcon from '../assets/instagram.png'; // Importa el ícono de Instagram
-import affiliateIcon from '../assets/affiliate.png'; // Importa el ícono de Afiliarse
+import affiliateIcon from '../assets/affiliate.png'; // Importa el ícono de "Afiliarse"
 
 const Navbar = () => {
   const whatsappNumber = '543534296125'; // Cambia este número al de tu empresa
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <header className="navbar-container">
       <div className="logo-container">
-        {/* Contenedor de los íconos */}
+        {/* Contenedor de los íconos de redes sociales */}
         <div className="icon-container">
           <a href={`https://wa.me/${whatsappNumber}`} target="_blank" rel="noopener noreferrer">
             <img src={whatsappIcon} alt="WhatsApp Icon" className="whatsapp-icon" />
@@ -25,10 +25,12 @@ const Navbar = () => {
         <img src={logo} alt="Logo Uniendo Villa María" className="logo" />
         {/* Contenedor para el ícono de Afiliarse */}
         <div className="affiliate-container">
-          <a href="#" className="affiliate-link">
-            <img src={affiliateIcon} alt="Afiliarse Icon" className="affiliate-icon" />
-            <span className="affiliate-text">Afiliarse</span>
-          </a>
+          <div className="affiliate-box">
+            <a href="/affiliate" className="affiliate-link">
+              <img src={affiliateIcon} alt="Afiliarse" className="affiliate-icon" />
+              <span className="affiliate-text">Afiliarse</span>
+            </a>
+          </div>
         </div>
       </div>
       <nav className="navbar">
